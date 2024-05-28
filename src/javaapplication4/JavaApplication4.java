18496df0372742a8b61058c82723c2ebacc31b15
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-05-28 21:54:12 +0300

    gg

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -14,7 +14,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hhh");
 
     }
     
