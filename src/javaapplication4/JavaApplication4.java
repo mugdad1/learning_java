caa13b3d051bd5a957a5f1c6570591cbb622eb9c
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-05-28 21:48:03 +0300

    test1
    test2

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author mugdad
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+    
+}
