29ee8bafe63bc85fce2faffe4b13210d468a857b
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-05-29 05:36:54 +0300

    tesr

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -14,7 +14,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("nicccccccccccccccccce");
+        System.out.println("ukjhc");
 
     }
     
