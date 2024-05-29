40022170cd88ec5d7857a9947c1e8cf881712b0e
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-05-29 05:04:31 +0300

    Update JavaApplication4.java

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -14,7 +14,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("goodnight");
+        System.out.println("goodmorning");
 
     }
     
