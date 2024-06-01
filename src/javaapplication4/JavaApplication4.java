36b40fecda7f11b964a29861616aff784aa800a3
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-06-01 07:36:46 +0300

    hi

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -14,7 +14,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("heaven from linux");
+        System.out.println("heaven from netbeans");
 
     }
     
