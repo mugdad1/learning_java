f95bda1ee4a384d6ac73eef1cf79223addf8fc3b
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-06-04 06:52:41 +0300

    new leesson

---
+++ src/javaapplication4/HelloJavs.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author mugdad
+ */
+public class HelloJavs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("heaven from netbeans");
+        System.out.println("heaven in netbeans");
+        System.out.println("hello world");
+        
+
+
+    }
+    
+}
