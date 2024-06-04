99a62936748fc0dcced88e299df10ace4148a368
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-06-04 22:12:58 +0300

    added day 1

---
+++ src/Bro_code/Expression.java
@@ -0,0 +1,19 @@
+//psvm for     public static void main(String[] args) {
+//alt shift f for orgnize
+//f6 for run
+package Bro_code;
+
+/**
+ *
+ * @author mugdad
+ */
+public class Expression {
+
+    public static void main(String[] args) {
+        int friends = 10;
+        friends += 1;
+        System.out.println(friends);
+        double friend = (double) friends / 3;
+        System.out.println(friend);
+    }
+}
