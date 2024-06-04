6f3bdb65a0e1354db43b95fed2b70a8497438fec
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-06-04 06:53:02 +0300

    new leesson

---
+++ src/javaapplication4/lessson2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author mugdad
+ */
+public class lessson2 {
+    public static void main(String[] args){
+        
+    System.out.println("hell from netbeans");
+
+    }
+    
+}
