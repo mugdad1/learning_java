96c01e2b20337dd9eecc07a23fcf5b6bd8d51c18
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-06-04 08:28:10 +0300

    hi

--- src/javaapplication4/HelloWorld.java
+++ src/javaapplication4/HelloWorld.java
@@ -8,18 +8,31 @@ package javaapplication4;
  *
  * @author mugdad
  */
-public class HelloJavs {
+public class HelloWorld {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("heaven from netbeans");
-        System.out.println("heaven in netbeans");
+        System.out.println("heaven from netbeans\n");
+        System.out.print("heaven in netbeans\t");
         System.out.println("hello world");
+        System.out.println("4");
+
+        
+                
+                
+                
         
 
 
     }
     
 }
+
+//println for new line
+/*
+\n for newline 
+\t tab
+\\ 
+*/
---
+++ src/javaapplication4/Swap2Vars.java
@@ -0,0 +1,29 @@
+//psvm for     public static void main(String[] args) {
+//alt shift f for orgnize
+//f6 for run
+package javaapplication4;
+
+/**
+ *
+ * @author mugdad
+ */
+public class Swap2Vars {
+
+    public static void main(String[] args) {
+        String x = "water ";
+        String y = "kool kid";
+        String z;
+        //z = x + y;
+        z=x;//x is 
+        x=y;
+        y=z;
+        
+
+        System.out.println(x + y);
+        System.out.println(y);
+        System.out.println(x);
+        System.out.println(z);
+
+    }
+
+}
