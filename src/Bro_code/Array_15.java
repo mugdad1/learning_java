03495f3619828d9e33134a31958e39f81b4a4cb2
mugdad <128133561+mugdad1@users.noreply.github.com>
2024-06-10 18:04:11 +0300

    m

---
+++ src/Bro_code/Array_15.java
@@ -0,0 +1,20 @@
+//psvm forpublic static void main(String[] args) {
+//alt shift f for orgnize
+//f6 for run
+//sout for print
+package Bro_code;
+
+/**
+ *
+ * @author mugdad
+ */
+public class Array_15 {
+
+    public static void main(String[] args) {
+        String[] cars = {"camero", "porche","mustang","sunny","tesla"};
+        cars[0] = "hello";
+        System.out.println(cars[0]);
+
+    }
+
+}
