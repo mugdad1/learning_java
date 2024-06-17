28dffaf5cc642d745ec02657c293ca930f471770
mugdad1 <128133561+mugdad1@users.noreply.github.com>
2024-06-17 16:05:36 +0300

    18

--- src/Bro_code/StringMethods_17.java
+++ src/Bro_code/StringMethods_17.java
@@ -9,10 +9,19 @@ package Bro_code;
  * @author mugdad
  */
 public class StringMethods_17 {
+
     public static void main(String[] args) {
-        String name="bro";
-       boolean result = name.equals("nihhs");
-        System.out.println(result);
+        String name = "   Bro    ";
+        //boolean result = name.equals("nihhs");//check if name is the variable is the same as the between parantheses()
+        //int result = name.length();//finds the length of the char
+        //char result = name.charAt(1);//finds the char at specific index
+        //int result= name.indexOf("o");
+        //boolean result = name.isEmpty();
+        //String result = name.toLowerCase(); //change all to lower case
+        //String result = name.replace("r", "M");//replace 1 with 2
+        //String result = name.substring(1,3);//gets the string from the index to the index
+        String result = name.trim();//removes spaces from begging and last
+        System.out.println(result);//prints the result
     }
-    
+
 }
