c397616a18650491990d4e9dd9ea6522ea5446a9
mugdad <mugdad02@tutamail.com>
2026-01-20 14:01:49 +0300

    back again

--- First 25/src/First25/HelloWorld_1.java
+++ First 25/src/First25/HelloWorld_1.java
@@ -14,8 +14,8 @@ public class HelloWorld_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("heaven from netbeans\n");
-        System.out.print("heaven in netbeans\t");
+        System.out.println("heaven from netbeans");
+        System.out.println("heaven in netbeans");
         System.out.println("hello world");
         System.out.println("4");
 
