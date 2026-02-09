5c677c46eb3cb3fdaf70b27e22fbf54557c23bc9
mugdad <mugdad02@tutamail.com>
2026-02-09 09:20:11 +0300

    j

--- OOP2/lab4/src/MyJFrame.java
+++ OOP2/lab4/src/MyJFrame.java
@@ -5,35 +5,41 @@ import java.awt.event.ActionListener;
 import java.beans.EventHandler;
 
 public class MyJFrame extends JFrame {
-    JLabel num1Lb1 = new JLabel ("number1 : ");
-    JLabel num2Lb1 = new JLabel ("number2 : ");
-    JLabel rsltLb1 = new JLabel ("the result : ");
+    JLabel num1Lb1 = new JLabel("number1 : ");
+    JLabel num2Lb1 = new JLabel("number2 : ");
     JTextField num1Txt = new JTextField(15);
     JTextField num2Txt = new JTextField(15);
-    JTextField rsltTxt = new JTextField(15);
+    JButton addbtn = new JButton("ADD");
+    JButton subbtn = new JButton("sub");
     EventHandler listener = new EventHandler();
 
-    public MyJFrame(){
+    public MyJFrame() {
         super("add 2 num");
-        setLayout (new FlowLayout());
+        setLayout(new FlowLayout());
         add(num1Lb1);
         add(num1Txt);
         add(num2Lb1);
         add(num2Txt);
-        add(rsltLb1);
-        add(rsltTxt);
-        num2Txt.addActionListener(listener);
+        add(addbtn);
+        add(subbtn);
+        addbtn.addActionListener(listener);
+        subbtn.addActionListener(listener);
+
 
     }
+
 private class EventHandler implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
     int num1 = Integer.parseInt(num1Txt.getText());
     int num2 = Integer.parseInt(num2Txt.getText());
-    int result = num2 + num1 ;
-    rsltTxt.setText("result "+ result);
-
+    int result ;
+    if (e.getSource()==addbtn)
+        {result = num1 + num2;}
+    else
+    {result = num1-num2;}
+    JOptionPane.showMessageDialog(null , result , "Result",JOptionPane.INFORMATION_MESSAGE);
     }
 }
 
